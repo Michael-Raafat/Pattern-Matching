05364bb216f658df6e61ff1eb1d4240b450443db
Michael <micoraafat96@gmail.com>
2021-05-19 13:20:24 -0400

    First Commit, project setup

---
+++ Pattern Matching/src/algorithms/AlgorithmsImp.java
@@ -0,0 +1,5 @@
+package algorithms;
+
+public class AlgorithmsImp implements AlgorithmsInf {
+
+}
---
+++ Pattern Matching/src/main/MatchingRunner.java
@@ -0,0 +1,9 @@
+package main;
+
+public class MatchingRunner {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
